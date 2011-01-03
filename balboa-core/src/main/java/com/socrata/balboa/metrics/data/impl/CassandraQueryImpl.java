9130d9092359bd83b6438d8ad3a7df3d1652f15a
Sam Gibson <sam.gibson@socrata.com>
2011-01-03 15:44:03 -0800

    NOOP: Refactor CQI to reduce hector dependency.

--- balboa-core/src/main/java/com/socrata/balboa/metrics/data/impl/CassandraQueryImpl.java
+++ balboa-core/src/main/java/com/socrata/balboa/metrics/data/impl/CassandraQueryImpl.java
@@ -5,13 +5,14 @@ import com.socrata.balboa.metrics.data.DateRange;
 import me.prettyprint.cassandra.service.CassandraClient;
 import me.prettyprint.cassandra.service.CassandraClientPool;
 import me.prettyprint.cassandra.service.CassandraClientPoolFactory;
-import me.prettyprint.cassandra.service.Keyspace;
 import org.apache.cassandra.thrift.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * An implementation of CassandraQuery that actually speaks with cassandra.
@@ -36,11 +37,8 @@ public class CassandraQueryImpl implements CassandraQuery
         pool = CassandraClientPoolFactory.getInstance().get();
     }
 
-    @Override
-    public List<Column> getMeta(String entityId) throws IOException
+    CassandraClient getClient()
     {
-        long startTime = System.currentTimeMillis();
-
         CassandraClient client;
         try
         {
@@ -57,18 +55,32 @@ public class CassandraQueryImpl implements CassandraQuery
             throw new CassandraDataStore.CassandraQueryException("Unknown exception trying to borrow a cassandra client.", e);
         }
 
-        Keyspace keyspace = null;
+        return client;
+    }
+
+    void releaseClient(CassandraClient client) throws IOException
+    {
+        try
+        {
+            pool.releaseClient(client);
+        }
+        catch (Exception e)
+        {
+            log.warn("Unable to release the cassandra client for some reason (not good).", e);
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public List<Column> getMeta(String entityId) throws IOException
+    {
+        long startTime = System.currentTimeMillis();
+
+        CassandraClient hectorClient = getClient();
+        Cassandra.Client client = hectorClient.getCassandra();
 
         try
         {
-            long keyspaceStartTime = System.currentTimeMillis();
-            keyspace = client.getKeyspace(keyspaceName, ConsistencyLevel.QUORUM, CassandraClient.FailoverPolicy.FAIL_FAST);
-            long totalKeyspaceTime = System.currentTimeMillis() - keyspaceStartTime;
-            if (totalKeyspaceTime >= KEYSPACE_BORROW_THRESHOLD)
-            {
-                log.warn("Slow getting a keyspace for reading from cassandra " + totalKeyspaceTime + " (ms).");
-            }
-
             SlicePredicate predicate = new SlicePredicate();
             SliceRange range = new SliceRange("".getBytes(), "".getBytes(), false, 500);
             predicate.setSlice_range(range);
@@ -76,19 +88,30 @@ public class CassandraQueryImpl implements CassandraQuery
 
             while (true)
             {
-                List<Column> queryResults = keyspace.getSlice(entityId, new ColumnParent("meta"), predicate);
+                List<ColumnOrSuperColumn> queryResults = client.get_slice(keyspaceName, entityId, new ColumnParent("meta"), predicate, ConsistencyLevel.QUORUM);
+
+                if (queryResults == null)
+                {
+                    break;
+                }
+
+                List<Column> columns = new ArrayList<Column>(queryResults.size());
+                for (ColumnOrSuperColumn columnOrSuperColumn : queryResults)
+                {
+                    columns.add(columnOrSuperColumn.getColumn());
+                }
 
                 // Update the range so that the next time we fill the buffer, we
                 // do it starting from the last of the returned results.
-                if (queryResults != null && queryResults.size() > 1)
+                if (queryResults.size() > 1)
                 {
-                    Column last = queryResults.remove(queryResults.size() - 1);
+                    Column last = columns.remove(columns.size() - 1);
                     range.setStart(last.getName());
-                    results.addAll(queryResults);
+                    results.addAll(columns);
                 }
-                else if (queryResults != null && queryResults.size() == 1)
+                else if (queryResults.size() == 1)
                 {
-                    results.addAll(queryResults);
+                    results.addAll(columns);
                     break;
                 }
                 else
@@ -98,10 +121,6 @@ public class CassandraQueryImpl implements CassandraQuery
             }
 
             return results;
-        }
-        catch (NotFoundException e)
-        {
-            throw new IOException("Keyspace '" + keyspaceName + "' not found.");
         }
         catch (InvalidRequestException e)
         {
@@ -115,28 +134,10 @@ public class CassandraQueryImpl implements CassandraQuery
         }
         finally
         {
-            IOException possiblyThrown = null;
-
-            try
-            {
-                pool.releaseClient(keyspace == null ? client : keyspace.getClient());
-            }
-            catch (Exception e)
-            {
-                log.warn("Unable to release the cassandra client for some reason (not good).", e);
-                possiblyThrown = new IOException(e);
-            }
-
             long totalTime = System.currentTimeMillis() - startTime;
-            log.debug("Total read time for '" + entityId + "' to cassandra " + totalTime + " (ms).");
+            log.debug("Total read time for entity meta on '" + entityId + "' to cassandra " + totalTime + " (ms).");
 
-            if (possiblyThrown != null)
-            {
-                // Doesn't really matter which one is thrown, we just want to
-                // chuck out any exceptions and let someone smarter than us
-                // deal with them.
-                throw possiblyThrown;
-            }
+            releaseClient(hectorClient);
         }
     }
 
@@ -145,39 +146,20 @@ public class CassandraQueryImpl implements CassandraQuery
     {
         long startTime = System.currentTimeMillis();
         
-        CassandraClient client;
-        try
-        {
-            long clientStartTime = System.currentTimeMillis();
-            client = pool.borrowClient(servers);
-            long totalClientTime = System.currentTimeMillis() - clientStartTime;
-            if (totalClientTime >= CLIENT_BORROW_THRESHOLD)
-            {
-                log.warn("Slow borrowing a client from the pool " + totalClientTime + " (ms).");
-            }
-        }
-        catch (Exception e)
-        {
-            throw new CassandraDataStore.CassandraQueryException("Unknown exception trying to borrow a cassandra client.", e);
-        }
-
-        Keyspace keyspace = null;
+        CassandraClient hectorClient = getClient();
+        Cassandra.Client client = hectorClient.getCassandra();
 
         try
         {
-            long keyspaceStartTime = System.currentTimeMillis();
-            keyspace = client.getKeyspace(keyspaceName, ConsistencyLevel.QUORUM, CassandraClient.FailoverPolicy.FAIL_FAST);
-            long totalKeyspaceTime = System.currentTimeMillis() - keyspaceStartTime;
-            if (totalKeyspaceTime >= KEYSPACE_BORROW_THRESHOLD)
+            List<ColumnOrSuperColumn> results = client.get_slice(keyspaceName, entityId, new ColumnParent(period.toString()), predicate, ConsistencyLevel.QUORUM);
+            List<SuperColumn> superColumns = new ArrayList<SuperColumn>(results.size());
+
+            for (ColumnOrSuperColumn result : results)
             {
-                log.warn("Slow getting a keyspace for reading from cassandra " + totalKeyspaceTime + " (ms).");
+                superColumns.add(result.getSuper_column());
             }
 
-            return keyspace.getSuperSlice(entityId, new ColumnParent(period.toString()), predicate);
-        }
-        catch (NotFoundException e)
-        {
-            throw new IOException("Keyspace '" + keyspaceName + "' not found.");
+            return superColumns;
         }
         catch (Exception e)
         {
@@ -185,28 +167,10 @@ public class CassandraQueryImpl implements CassandraQuery
         }
         finally
         {
-            IOException possiblyThrown = null;
-
-            try
-            {
-                pool.releaseClient(keyspace == null ? client : keyspace.getClient());
-            }
-            catch (Exception e)
-            {
-                log.warn("Unable to release the cassandra client for some reason (not good).", e);
-                possiblyThrown = new IOException(e);
-            }
-
             long totalTime = System.currentTimeMillis() - startTime;
-            log.debug("Total read time for '" + entityId + "' to cassandra " + totalTime + " (ms).");
+            log.debug("Total read time for '" + entityId + "' (" + period + ") to cassandra " + totalTime + " (ms).");
 
-            if (possiblyThrown != null)
-            {
-                // Doesn't really matter which one is thrown, we just want to
-                // chuck out any exceptions and let someone smarter than us
-                // deal with them.
-                throw possiblyThrown;
-            }
+            releaseClient(hectorClient);
         }
     }
 
@@ -215,21 +179,12 @@ public class CassandraQueryImpl implements CassandraQuery
     {
         long startTime = System.currentTimeMillis();
         
-        CassandraClient client;
-        try
-        {
-            client = pool.borrowClient(servers);
-        }
-        catch (Exception e)
-        {
-            throw new CassandraDataStore.CassandraQueryException("Unknown exception trying to borrow a cassandra client.", e);
-        }
-
-        Keyspace keyspace = null;
+        CassandraClient hectorClient = getClient();
+        Cassandra.Client client = hectorClient.getCassandra();
 
         try
         {
-            client.getCassandra().batch_insert(keyspaceName, entityId, operations, ConsistencyLevel.QUORUM);
+            client.batch_insert(keyspaceName, entityId, operations, ConsistencyLevel.QUORUM);
         }
         catch (Exception e)
         {
@@ -237,28 +192,10 @@ public class CassandraQueryImpl implements CassandraQuery
         }
         finally
         {
-            IOException possiblyThrown = null;
-
-            try
-            {
-                pool.releaseClient(client);
-            }
-            catch (Exception e)
-            {
-                log.warn("Unable to release the cassandra client for some reason (not good).", e);
-                possiblyThrown = new IOException(e);
-            }
-
             long totalTime = System.currentTimeMillis() - startTime;
-            log.debug("Total write time for '" + entityId + "' to cassandra (not including lock) " + totalTime + " (ms)");
+            log.debug("Total write time for '" + entityId + "' (all periods and meta, not including lock) to cassandra " + totalTime + " (ms)");
 
-            if (possiblyThrown != null)
-            {
-                // Doesn't really matter which one is thrown, we just want to
-                // chuck out any exceptions and let someone smarter than us
-                // deal with them.
-                throw possiblyThrown;
-            }
+            releaseClient(hectorClient);
         }
     }
 
@@ -267,21 +204,8 @@ public class CassandraQueryImpl implements CassandraQuery
     {
         long startTime = System.currentTimeMillis();
 
-        CassandraClient client;
-        try
-        {
-            long clientStartTime = System.currentTimeMillis();
-            client = pool.borrowClient(servers);
-            long totalClientTime = System.currentTimeMillis() - clientStartTime;
-            if (totalClientTime >= CLIENT_BORROW_THRESHOLD)
-            {
-                log.warn("Slow borrowing a client from the pool " + totalClientTime + " (ms).");
-            }
-        }
-        catch (Exception e)
-        {
-            throw new CassandraDataStore.CassandraQueryException("Unknown exception trying to borrow a cassandra client.", e);
-        }
+        CassandraClient hectorClient = getClient();
+        Cassandra.Client client = hectorClient.getCassandra();
 
         try
         {
@@ -292,7 +216,7 @@ public class CassandraQueryImpl implements CassandraQuery
             sliceRange.setFinish(CassandraUtils.packLong(0));
             predicate.setSlice_range(sliceRange);
 
-            return client.getCassandra().get_range_slices(
+            return client.get_range_slices(
                     keyspaceName,
                     new ColumnParent(columnFamily),
                     predicate,
@@ -306,28 +230,10 @@ public class CassandraQueryImpl implements CassandraQuery
         }
         finally
         {
-            IOException possiblyThrown = null;
-
-            try
-            {
-                pool.releaseClient(client);
-            }
-            catch (Exception e)
-            {
-                log.warn("Unable to release the cassandra client for some reason (not good).", e);
-                possiblyThrown = new IOException(e);
-            }
-
             long totalTime = System.currentTimeMillis() - startTime;
             log.debug("Total read time for keyscan between " + range.getStart_key() + " and " + range.getEnd_key() + " to cassandra " + totalTime + " (ms).");
 
-            if (possiblyThrown != null)
-            {
-                // Doesn't really matter which one is thrown, we just want to
-                // chuck out any exceptions and let someone smarter than us
-                // deal with them.
-                throw possiblyThrown;
-            }
+            releaseClient(hectorClient);
         }
     }
 }
