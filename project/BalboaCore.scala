47d75be00ad5d2e39c004986ef31bf0397a5267b
Robert J. Macomber <robert.macomber@socrata.com>
2012-09-13 17:25:01 -0700

    SPARTA: Logging
    Remove commons-logging, add slf4j-log4j

--- project/BalboaCore.scala
+++ project/BalboaCore.scala
@@ -6,19 +6,21 @@ import SocrataSbtKeys._
 
 object BalboaCore {
   lazy val settings: Seq[Setting[_]] = BuildSettings.buildSettings ++ socrataProjectSettings() ++ Seq(
-    libraryDependencies ++= Seq(
-      "junit" % "junit" % "4.5" % "test",
-      "com.google.protobuf" % "protobuf-java" % "2.3.0",
-      "org.codehaus.jackson" % "jackson-core-lgpl" % "1.6.0",
-      "org.codehaus.jackson" % "jackson-mapper-lgpl" % "1.6.0",
-      "org.slf4j" % "slf4j-api" % "1.6.1",
-      "commons-logging" % "commons-logging" % "1.1.1",
-      "commons-collections" % "commons-collections" % "3.1",
-      "commons-dbcp" % "commons-dbcp" % "1.2.2",
-      "org.perf4j" % "perf4j" % "0.9.13",
-      "com.yammer" % "metrics_2.8.1" % "2.0.0-BETA10",
-      "log4j" % "log4j" % "1.2.16"
-    ),
+    libraryDependencies <++= (slf4jVersion) { slf4jVersion =>
+      Seq(
+        "junit" % "junit" % "4.5" % "test",
+        "com.google.protobuf" % "protobuf-java" % "2.3.0",
+        "org.codehaus.jackson" % "jackson-core-lgpl" % "1.6.0",
+        "org.codehaus.jackson" % "jackson-mapper-lgpl" % "1.6.0",
+        "org.slf4j" % "slf4j-api" % slf4jVersion,
+        "org.slf4j" % "slf4j-log4j12" % slf4jVersion,
+        "commons-collections" % "commons-collections" % "3.1",
+        "commons-dbcp" % "commons-dbcp" % "1.2.2",
+        "org.perf4j" % "perf4j" % "0.9.13",
+        "com.yammer" % "metrics_2.8.1" % "2.0.0-BETA10",
+        "log4j" % "log4j" % "1.2.16"
+      )
+    },
     dependenciesSnippet :=
       <xml.group>
         <exclude org="org.codehaus.jackson" module="jackson-mapper-asl" />
