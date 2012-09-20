a84271e6ede886947b5b957e925f66043b244d4a
Marc Millstone <marc.millstone@socrata.com>
2012-09-20 13:51:32 -0700

    Added scala jar as a dependency

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,6 @@ import com.socrata.socratasbt.CheckClasspath
 
 object BuildSettings {
   val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ socrataBuildSettings ++ Seq(
-    autoScalaLibrary := false,
     testOptions in Test += Tests.Setup( () => sys.props("socrata.env") = "test" ),
     testOptions in Test += Tests.Cleanup( () => sys.props -= "socrata.env" ),
     artifactName := { (_: ScalaVersion, _: ModuleID, artifact: Artifact) => artifact.name + "." + artifact.extension },
