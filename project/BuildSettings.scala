b2e901cb0f6e6c1ebed319fb566cce9a32cf7a28
Robert J. Macomber <robert.macomber@socrata.com>
2012-10-19 14:01:16 -0700

    BUILD: Fork a new JVM to run the tests in
    Reviewed-by: john.kew@socrata.com
    
    Conflicts:
    	project/BuildSettings.scala
    	project/build.sbt

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,9 +6,8 @@ import com.socrata.socratasbt.CheckClasspath
 
 object BuildSettings {
   val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ socrataBuildSettings ++ Seq(
-    testOptions in Test += Tests.Setup( () => sys.props("socrata.env") = "test" ),
-    testOptions in Test += Tests.Cleanup( () => sys.props -= "socrata.env" ),
     artifactName := { (_: ScalaVersion, _: ModuleID, artifact: Artifact) => artifact.name + "." + artifact.extension },
-    parallelExecution in Test := false
+    fork in test := true,
+    javaOptions in test += "-Dsocrata.env=test"
   )
 }
