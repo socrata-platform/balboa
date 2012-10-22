e83b3444df1a77468b26773464c3ce25779d6f8e
John Kew <john.kew@socrata.com>
2012-10-22 14:11:49 -0700

    FEATURE: Only dump data which exists within the range of the largest gran + some    additional error checking on dumped data.
    QA: None
    
    Reviewed-by: marc.millstone@socrata.com>
    
    Conflicts:
    
    	balboa-admin/src/main/java/com/socrata/balboa/admin/tools/Dumper.java

--- balboa-admin/src/main/java/com/socrata/balboa/admin/tools/Dumper.java
+++ balboa-admin/src/main/java/com/socrata/balboa/admin/tools/Dumper.java
@@ -18,8 +18,9 @@ public class Dumper
         this.writer = writer;
     }
 
-    void output(String entityId, Iterator<Timeslice> iter, CSVWriter writer)
+    int output(String entityId, Iterator<Timeslice> iter, CSVWriter writer)
     {
+        Set<String> metricNames = new HashSet<String>();
         while (iter.hasNext())
         {
             Timeslice slice = iter.next();
@@ -27,6 +28,7 @@ public class Dumper
 
             for (Map.Entry<String, Metric> metric : slice.getMetrics().entrySet())
             {
+                metricNames.add(metric.getKey());
                 writer.writeNext(new String[] {
                         entityId,
                         Long.toString(timestamp),
@@ -36,15 +38,34 @@ public class Dumper
                 });
             }
         }
+        return metricNames.size();
+    }
+
+    // Dump only a specific entity
+    public void dumpOnly(String entityId) throws IOException {
+        Period mostGranular = Period.mostGranular(Configuration.get().getSupportedPeriods());
+        Period leastGranular = Period.leastGranular(Configuration.get().getSupportedPeriods());
+        Date epoch = new Date(0);
+        Date cutoff = DateRange.create(leastGranular, new Date()).end;
+        DataStore ds = DataStoreFactory.get();
+        Iterator<Timeslice> bigSlices = ds.slices(entityId, leastGranular, epoch, cutoff);
+        while(bigSlices.hasNext()) {
+            Timeslice current = bigSlices.next();
+            int expected = current.getMetrics().size();
+            if (expected > 0) {
+                Date start = new Date(current.getStart());
+                cutoff = new Date(current.getEnd());
+                Iterator<Timeslice> slices = ds.slices(entityId, mostGranular, start, cutoff);
+                int got = output(entityId, slices, writer);
+                if (expected != got) {
+                    new IllegalStateException("Did not get the right number of metrics for entity " + entityId + " expected: " + expected + " got: " + got);
+                }
+            }
+        }
     }
 
     public void dump(List<String> filters) throws IOException
     {
-        Period mostGranular = Period.mostGranular(Configuration.get().getSupportedPeriods());
-
-        Date epoch = new Date(0);
-        Date cutoff = DateRange.create(mostGranular, new Date()).start;
-
         DataStore ds = DataStoreFactory.get();
         Iterator<String> entities;
         if (filters.size() > 0)
@@ -63,8 +84,7 @@ public class Dumper
         while (entities.hasNext())
         {
             String entityId = entities.next();
-            Iterator<Timeslice> slices = ds.slices(entityId, mostGranular, epoch, cutoff);
-            output(entityId, slices, writer);
+            dumpOnly(entityId);
         }
     }
 }
