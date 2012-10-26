d94973db370c809fdaae59b43078b436054cf8bf
John Kew <john.kew@socrata.com>
2012-10-26 16:13:35 -0700

    Fix up the start script so it does not reference maven.

--- balboa.sh
+++ balboa.sh
@@ -8,13 +8,14 @@ shift
 
 case "$cmd" in
   http)
-    mvn install
-    cd balboa-http
-    exec mvn jetty:run -Djetty.port=9898 "$@"
+    sbt test update package assembly
+    echo -e "Do:
+>sbt
+balboa:0.2:cassandra11-squash> project balboa-http
+balboa-http:0.2:cassandra11-squash> container:start"
     ;;
   jms)
-    mvn install
-    cd balboa-jms
+    sbt test update package assembly
     #exec java -agentpath:/Applications/YourKit_Java_Profiler_9.5.6.app/bin/mac/libyjpagent.jnilib -jar target/balboa-jms-jar-with-dependencies.jar 2 failover:tcp://10.0.2.135:61616 Metrics2
     exec java -jar target/balboa-jms-jar-with-dependencies.jar 2 failover:tcp://localhost:61616 Metrics2
     ;;
