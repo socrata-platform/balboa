46a3fe028c8d59871a7b46757eda25cfd8b0e50f
Robert J. Macomber <robert.macomber@socrata.com>
2012-12-17 15:14:51 -0800

    FEATURE: Only read the version string once, and set a content-type
    QA: No
    
    Reviewed-by: john.kew@socrata.com

--- balboa-http/src/main/scala/com/socrata/balboa/server/rest/VersionRest.scala
+++ balboa-http/src/main/scala/com/socrata/balboa/server/rest/VersionRest.scala
@@ -11,17 +11,13 @@ import com.rojoma.simplearm.util._
  * Version/Health-Check
  */
 object VersionRest extends Service[HttpServletRequest, HttpResponse] {
+  val versionString = for {
+    resource <- managed(getClass.getClassLoader.getResourceAsStream("version"))
+    source <- managed(scala.io.Source.fromInputStream(resource, "UTF-8"))
+  } yield source.mkString
+
   def apply(request: HttpServletRequest): HttpResponse = {
-    val inFastFail = BalboaFastFailCheck.getInstance().isInFailureMode
-    val versionString = for {
-      resource <- managed(getClass.getClassLoader.getResourceAsStream("version"))
-      source <- managed(scala.io.Source.fromInputStream(resource, "UTF-8"))
-    } yield source.mkString
-
-    if (inFastFail)
-      InternalServerError ~> Content(versionString)
-    else
-      OK ~> Content(versionString)
-
+    val responseCode = if(BalboaFastFailCheck.getInstance.isInFailureMode) InternalServerError else OK
+    responseCode ~> ContentType("application/json") ~> Content(versionString)
   }
 }
