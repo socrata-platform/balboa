afd67c12d9c42c043b263efb708c59dcd98a4c8e
Michael Hotan <michael.hotan@socrata.com>
2015-02-26 14:38:52 -0800

    Added small build task for moving jar into docker folder.  Version bump for balboa-kaka

--- project/BalboaKafka.scala
+++ project/BalboaKafka.scala
@@ -1,4 +1,3 @@
-import com.socrata.cloudbeessbt.SocrataCloudbeesSbt.SocrataSbtKeys._
 import sbt.Keys._
 import sbt._
 
@@ -6,14 +5,26 @@ import sbtassembly.Plugin.AssemblyKeys._
 
 object BalboaKafka {
 
-  lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings(assembly = true) ++ Seq(
-    mainClass in assembly := Some("com.socrata.balboa.kafka.Main"),
-    libraryDependencies ++= Seq(
-      "org.mockito" % "mockito-core" % "1.+" % "test",
-      "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-      "org.apache.kafka" %% "kafka" % "0.8.2.0",
-      "commons-logging" % "commons-logging" % "1.1.1",
-      "com.github.scopt" %% "scopt" % "3.3.0"
-    )
-  )
+  val stageDocker = TaskKey[Unit]("stageDocker", "Copies assembly jar to docker directory")
+
+  // Task that handles migrating the assembly jar to docker directory for staging
+  val stageDockerTask = stageDocker <<= assembly map { (f) =>
+    println("Staging file for Docker: " + Keys.version)
+    // TODO Remove current version and replace with LATEST wtf why can't I do this
+    val dockerDir = new File("docker")
+    dockerDir.mkdir()
+    IO.copyFile(f, dockerDir / f.getName)
+  }
+
+  lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings(assembly = true) ++
+    Seq(mainClass in assembly :=
+      Some("com.socrata.balboa.kafka.Main"),
+      libraryDependencies ++= Seq(
+        "org.mockito" % "mockito-core" % "1.+" % "test",
+        "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+        "org.apache.kafka" %% "kafka" % "0.8.2.0",
+        "commons-logging" % "commons-logging" % "1.1.1",
+        "com.github.scopt" %% "scopt" % "3.3.0"
+      )
+    ) ++ Seq(stageDockerTask)
 }
