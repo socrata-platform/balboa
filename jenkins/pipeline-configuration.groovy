1bcd8fbbee106082c1dd2383f5120761c71c78a4
Jacob Simpson <jacob.simpson@socrata.com>
2016-04-21 15:40:20 -0700

    Add pipeline configuration.

---
+++ jenkins/pipeline-configuration.groovy
@@ -0,0 +1,7 @@
+// This is the configuration for the Jenkins build pipeline for this
+// repository. More information is available at:
+//   https://github.com/socrata/pipelines
+
+integration = [
+  on: false
+]
