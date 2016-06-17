924bc1e7090286797af08405d7e8a0ece7d2f401
Taylor Cramer <cramertj@github.com>
2016-06-17 13:37:21 -0700

    Move AMQ creation into function to allow for easier transition to HTTP

--- balboa-agent/src/main/scala/com/socrata/balboa/agent/BalboaAgent.scala
+++ balboa-agent/src/main/scala/com/socrata/balboa/agent/BalboaAgent.scala
@@ -8,6 +8,7 @@ import com.blist.metrics.impl.queue.MetricJmsQueueNotSingleton
 import com.codahale.metrics.JmxReporter
 import com.socrata.balboa.agent.metrics.BalboaAgentMetrics
 import com.socrata.balboa.util.FileUtils
+import com.socrata.metrics.MetricQueue
 import com.typesafe.scalalogging.slf4j.StrictLogging
 import org.apache.activemq.transport.TransportListener
 import org.apache.activemq.{ActiveMQConnection, ActiveMQConnectionFactory}
@@ -34,81 +35,17 @@ object BalboaAgent extends App with Config with StrictLogging {
   logger info "Starting the JMX Reporter."
   jmxReporter.start()
 
-  logger info "Initializing ActiveMQ connection factory. (This is setting the username, password and destination.)"
-  val amqConnectionFactory = (activemqUser, activemqPassword) match {
-    case (Some(user), Some(password)) => new ActiveMQConnectionFactory(user, password, activemqServer)
-    case _                            => new ActiveMQConnectionFactory(activemqServer)
+  private val reportStrategy = this.httpOrMQ()
+
+  val metricQueue = reportStrategy match {
+    case HttpOrMq.MQ => amqMetricQueue()
+    case HttpOrMq.HTTP => throw new RuntimeException("Metric reporting by HTTP not yet supported")
   }
-
-  // The ActiveMQ libraries can be obscure when they are misbehaving. The hope
-  // is that by registering this listener, additional information will be
-  // written to the logs for troubleshooting.
-  amqConnectionFactory.setTransportListener(new TransportListener {
-    // The onCommand handler seems to be a routine handler. No additional error
-    // reporting information is available by overriding it.
-    override def onCommand(command: scala.Any): Unit = {}
-
-    override def onException(error: IOException): Unit =
-      logger error("Problem with ActiveMQ connection.", error)
-
-    // This method appears to be invoked when the library is first initializing
-    // as well. So this log message will appear after the call to `.start` down
-    // below.
-    override def transportInterupted(): Unit =
-      logger warn "ActiveMQ connection is being closed or has suffered an interruption from which it hopes to recover."
-
-    override def transportResumed(): Unit =
-      logger warn "ActiveMQ connection is starting up or has resumed after an interruption."
-  })
-
-  // An ActiveAMQConnection already contains the logic to do exponential backoff
-  // and retry on failed connections. It will also automatically apply the same
-  // logic to reconnecting to a server when the connection is lost.
-  val amqConnection = try {
-    amqConnectionFactory.createConnection().asInstanceOf[ActiveMQConnection]
-  } catch {
-    // Known causes of an exception here are poorly formatted connection string.
-    // No amount of retry will fix that, so just exit.
-    case e: Throwable =>
-      logger error("Unable to initialize ActiveMQ connection.", e)
-      sys.exit(1)
-  }
-  logger info s"Initialized ActiveMQ connection ${amqConnection.getConnectionInfo}"
-
-  amqConnection.setExceptionListener(new ExceptionListener() {
-    override def onException(exception: JMSException): Unit =
-      logger error("ActiveMQ connection encountered an error.", exception)
-  })
-
-  logger info "Connecting to ActiveMQ broker. (This is the actual TCP connection.)"
-  try {
-    // This step is where the actual TCP connection is initiated. Everything up
-    // until now was basically making some objects. The 'start' method may never
-    // return if the destination server is unavailable. As far as I can tell from
-    // testing, this method contains the logic to retry failed connections. It
-    // has a backup built in, and once it gets up to a 30s interval, it just sits
-    // and retries connections every 30s for at least 20min. I didn't test any
-    // longer than that.
-    amqConnection.start()
-  } catch {
-    // It is unclear under what practical circumstances this exception can occur.
-    // Network related connection failures do not appear to lead to an exception,
-    // just infinite retry attempts within the AMQ libraries. Without
-    // understanding the causes of this exception, it would be a bad idea to
-    // have automatic retry of connection attempts that end in an exception.
-    case e: Throwable =>
-      logger error("Unable to connect to ActiveMQ broker.", e)
-      sys.exit(1)
-  }
-  logger info s"Connected to ActiveMQ broker '${amqConnection.getBrokerInfo}'."
-
-  logger info s"Reading metrics from directory '${dataDir.getAbsolutePath}'."
-  logger info s"Sending metrics to ActiveMQ queue '$activemqQueue'"
 
   val future: ScheduledFuture[_] = scheduler.scheduleWithFixedDelay(new Runnable {
     override def run(): Unit = {
       try {
-        val mc = new MetricConsumer(dataDir, new MetricJmsQueueNotSingleton(amqConnection, activemqQueue))
+        val mc = new MetricConsumer(dataDir, metricQueue)
         try {
           logger debug s"Attempting to run Metric Consumer $mc"
           mc.run() // Recursively reads all metric data files and writes them to a queue.
@@ -155,4 +92,78 @@ object BalboaAgent extends App with Config with StrictLogging {
     }
   })
 
+  def amqMetricQueue(): MetricQueue = {
+    logger info "Initializing ActiveMQ connection factory. (This is setting the username, password and destination.)"
+    val amqConnectionFactory = (activemqUser, activemqPassword) match {
+      case (Some(user), Some(password)) => new ActiveMQConnectionFactory(user, password, activemqServer)
+      case _                            => new ActiveMQConnectionFactory(activemqServer)
+    }
+
+    // The ActiveMQ libraries can be obscure when they are misbehaving. The hope
+    // is that by registering this listener, additional information will be
+    // written to the logs for troubleshooting.
+    amqConnectionFactory.setTransportListener(new TransportListener {
+      // The onCommand handler seems to be a routine handler. No additional error
+      // reporting information is available by overriding it.
+      override def onCommand(command: scala.Any): Unit = {}
+
+      override def onException(error: IOException): Unit =
+        logger error("Problem with ActiveMQ connection.", error)
+
+      // This method appears to be invoked when the library is first initializing
+      // as well. So this log message will appear after the call to `.start` down
+      // below.
+      override def transportInterupted(): Unit =
+        logger warn "ActiveMQ connection is being closed or has suffered an interruption from which it hopes to recover."
+
+      override def transportResumed(): Unit =
+        logger warn "ActiveMQ connection is starting up or has resumed after an interruption."
+    })
+
+    // An ActiveAMQConnection already contains the logic to do exponential backoff
+    // and retry on failed connections. It will also automatically apply the same
+    // logic to reconnecting to a server when the connection is lost.
+    val amqConnection = try {
+      amqConnectionFactory.createConnection().asInstanceOf[ActiveMQConnection]
+    } catch {
+      // Known causes of an exception here are poorly formatted connection string.
+      // No amount of retry will fix that, so just exit.
+      case e: Throwable =>
+        logger error("Unable to initialize ActiveMQ connection.", e)
+        sys.exit(1)
+    }
+    logger info s"Initialized ActiveMQ connection ${amqConnection.getConnectionInfo}"
+
+    amqConnection.setExceptionListener(new ExceptionListener() {
+      override def onException(exception: JMSException): Unit =
+        logger error("ActiveMQ connection encountered an error.", exception)
+    })
+
+    logger info "Connecting to ActiveMQ broker. (This is the actual TCP connection.)"
+    try {
+      // This step is where the actual TCP connection is initiated. Everything up
+      // until now was basically making some objects. The 'start' method may never
+      // return if the destination server is unavailable. As far as I can tell from
+      // testing, this method contains the logic to retry failed connections. It
+      // has a backup built in, and once it gets up to a 30s interval, it just sits
+      // and retries connections every 30s for at least 20min. I didn't test any
+      // longer than that.
+      amqConnection.start()
+    } catch {
+      // It is unclear under what practical circumstances this exception can occur.
+      // Network related connection failures do not appear to lead to an exception,
+      // just infinite retry attempts within the AMQ libraries. Without
+      // understanding the causes of this exception, it would be a bad idea to
+      // have automatic retry of connection attempts that end in an exception.
+      case e: Throwable =>
+        logger error("Unable to connect to ActiveMQ broker.", e)
+        sys.exit(1)
+    }
+    logger info s"Connected to ActiveMQ broker '${amqConnection.getBrokerInfo}'."
+
+    logger info s"Reading metrics from directory '${dataDir.getAbsolutePath}'."
+    logger info s"Sending metrics to ActiveMQ queue '$activemqQueue'"
+
+    new MetricJmsQueueNotSingleton(amqConnection, activemqQueue)
+  }
 }
