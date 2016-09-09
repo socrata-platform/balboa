44414ef216e330266bc11f36cdba0f9076d416f0
Taylor Cramer <cramertaylorj@gmail.com>
2016-09-09 11:00:38 -0700

    Healthcheck returns 503 when unhealthy (#164)

--- balboa-http/src/main/scala/com/socrata/balboa/server/HealthCheckServlet.scala
+++ balboa-http/src/main/scala/com/socrata/balboa/server/HealthCheckServlet.scala
@@ -1,5 +1,7 @@
 package com.socrata.balboa.server
 
+import javax.servlet.http.HttpServletResponse
+
 import com.socrata.balboa.BuildInfo
 import com.socrata.balboa.metrics.data.{DataStoreFactory, DefaultDataStoreFactory}
 import org.scalatra.ScalatraServlet
@@ -23,6 +25,13 @@ class HealthCheckServlet(dataStoreFactory: DataStoreFactory) extends ScalatraSer
     val serving = if (ClientCounter.get() > 0) { 1 } else { 0 }
 
     contentType = "text/plain"
+
+    status = if (cassandra == 1) {
+      HttpServletResponse.SC_OK
+    } else {
+      HttpServletResponse.SC_SERVICE_UNAVAILABLE
+    }
+
     s"""healthy: $cassandra
       |healthy.cassandra: $cassandra
       |serving: $serving
